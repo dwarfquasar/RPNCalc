59be9759cc062003fa685013ba7ef746ab2ae1b5
Owen Danke <39525303+dwarfquasar@users.noreply.github.com>
2023-02-02 09:29:48 -0500

    Add files via upload

---
+++ rpncalc.cpp
@@ -0,0 +1,42 @@
+/* Owen Danke */
+/* A Reverse Polish Notation Calculator */
+
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main(){
+
+float tf;
+stack<float> fs;
+
+
+cout << "Input number: ";
+cin >> tf;
+fs.push(tf);
+
+while(cin >> tf){
+
+float A, B;
+
+	switch(tf){
+		case isDigit:
+		
+		case "+":
+		// A B +
+		// A + B
+		
+		
+		case "-":
+		
+		case "*":
+		
+		case "/":
+		
+		
+		default:
+	}
+
+}
+
+}
